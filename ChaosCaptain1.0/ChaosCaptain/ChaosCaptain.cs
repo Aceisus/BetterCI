0a4bcfe31666178a10b3092bf1b49e1b20ec659b
Aceisus <80728843+Aceisus@users.noreply.github.com>
2021-03-18 15:44:15 -0700

    Add files via upload

---
+++ ChaosCaptain1.0/ChaosCaptain/ChaosCaptain.cs
@@ -0,0 +1,36 @@
+﻿using Synapse;
+using Synapse.Api.Roles;
+using Synapse.Api.Items;
+using System.Collections.Generic;
+
+namespace NewRole
+{
+    public class NewRoleName : Synapse.Api.Roles.Role
+    {
+        public override int GetRoleID() => 20; 
+
+        public override string GetRoleName() => "Chaos Captain";
+
+        public override Team GetTeam() => Team.CHI;
+
+        public override void Spawn() 
+        {
+            Player.RoleType = RoleType.ChaosInsurgency;
+            Player.MaxHealth = 200;
+            var item = new SynapseItem(ItemType.GunLogicer, 10, 0, 0, 3);
+            item.PickUp(Player);
+            
+        }
+        public override void DeSpawn()
+        {
+            Player.SendBroadcast(5, "You are no longer a Chaos Captain");
+        }
+
+        public override List<Team> GetFriends() => new List<Team> { Team.CDP, Team.RSC };
+
+
+        public virtual List<Team> GetEnemys() => new List<Team> { Team.MTF }; //This means this Role and MTF must kill each other or else the round will not end
+   
+    }
+
+}
---
+++ ChaosCaptain1.0/ChaosCaptain/Logicer2.cs
@@ -0,0 +1,25 @@
+﻿using Synapse;
+using Synapse.Api;
+using Synapse.Api.Plugin;
+
+namespace Synapse.Api.Items
+{
+    public class Logicer2 : AbstractPlugin
+    {
+
+        public override void Load()
+        {
+            var info = new Synapse.Api.Items.CustomItemInformation()
+            {
+                BasedItemType = ItemType.GunLogicer,
+                ID = 100,
+                Name = "Logicer-KR18",
+            
+            };
+
+            Server.Get.ItemManager.RegisterCustomItem(info);
+        }
+
+    }
+}
+
